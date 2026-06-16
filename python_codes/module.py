670a656cfb2182bd79ac891833d957d3aa83c16e
Aadibeniwal10 <beniwaladitya99@gmail.com>
2026-06-16 08:48:11 +0530

    Update module.py

--- python_codes/module.py
+++ python_codes/module.py
@@ -3,3 +3,11 @@ import pyjokes
 # This prints a random joke
 joke = pyjokes.get_joke()
 print(joke)
+
+
+
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey I am good")
+engine.runAndWait()
