5ff1d659790b99a46bb9440b85366dcc07302245
Aadibeniwal10 <beniwaladitya99@gmail.com>
2026-06-16 13:58:34 +0530

    Create variable.py

---
+++ python_codes/variable.py
@@ -0,0 +1,26 @@
+a = 1
+b = 2
+print(a + b)
+
+----
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and' 
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))
+
+----
+
+a = "31.2"
+b = float(a) # a but the type should be float
+t = type(b) 
+print(t)
