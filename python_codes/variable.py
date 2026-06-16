a26f775843ea152766e42e39d9454919b17633ba
Aadibeniwal10 <beniwaladitya99@gmail.com>
2026-06-16 14:04:21 +0530

    Update variable.py

--- python_codes/variable.py
+++ python_codes/variable.py
@@ -24,3 +24,30 @@ a = "31.2"
 b = float(a) # a but the type should be float
 t = type(b) 
 print(t)
+
+----
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+print("Number a is: ", a)
+print("Number b is: ", b) 
+print("Sum is ", a + b)
+
+----
+
+a = 37
+b = 5
+print("Remainder when a is divided by b is", a % b)
+
+----
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+print("The average of these two number is", (a+b)/2)
+
+----
+
+a = int(input("Enter your number: ")) 
+print("The square of the number is", a**2)
+print("The square of the number is", a*a)
+# print("The square of the number is", a^2) # Incorrect for finding square of a number in Python
